e36c8311e3282b7cc833c8af965cbf5de90f7307
Martin Vesterlund <Otyg@users.noreply.github.com>
2021-12-03 23:35:46 +0100

    Extract usage

--- model/types.go
+++ model/types.go
@@ -2,7 +2,6 @@ package model
 
 import (
 	"encoding/json"
-	"errors"
 	"fmt"
 	"regexp"
 	"sort"
@@ -122,39 +121,6 @@ func AuthenticationValues() []TypeEnum {
 func (what Authentication) String() string {
 	// NOTE: maintain list also in schema.json for validation in IDEs
 	return [...]string{"none", "credentials", "session-id", "token", "client-certificate", "two-factor", "externalized"}[what]
-}
-
-type Usage int
-
-const (
-	Business Usage = iota
-	DevOps
-)
-
-func UsageValues() []TypeEnum {
-	return []TypeEnum{
-		Business,
-		DevOps,
-	}
-}
-
-func ParseUsage(value string) (usage Usage, err error) {
-	value = strings.TrimSpace(value)
-	for _, candidate := range UsageValues() {
-		if candidate.String() == value {
-			return candidate.(Usage), err
-		}
-	}
-	return usage, errors.New("Unable to parse into type: " + value)
-}
-
-func (what Usage) String() string {
-	// NOTE: maintain list also in schema.json for validation in IDEs
-	return [...]string{"business", "devops"}[what]
-}
-
-func (what Usage) Title() string {
-	return [...]string{"Business", "DevOps"}[what]
 }
 
 type DataFormat int
