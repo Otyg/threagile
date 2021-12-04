af649ca776a480368a82efcf60f3c9483274afcd
Martin Vesterlund <Otyg@users.noreply.github.com>
2021-12-04 19:41:37 +0100

    CommunicationLink

--- model/communicationLink.go
+++ model/communicationLink.go
@@ -1,5 +1,7 @@
 package model
 
+import "sort"
+
 type InputCommunicationLink struct {
 	Target                   string   `json:"target"`
 	Description              string   `json:"description"`
@@ -35,4 +37,91 @@ func (what CommunicationLink) IsTaggedWithAny(tags ...string) bool {
 
 func (what CommunicationLink) IsTaggedWithBaseTag(basetag string) bool {
 	return IsTaggedWithBaseTag(what.Tags, basetag)
+}
+
+func (what CommunicationLink) IsAcrossTrustBoundary() bool {
+	trustBoundaryOfSourceAsset := DirectContainingTrustBoundaryMappedByTechnicalAssetId[what.SourceId]
+	trustBoundaryOfTargetAsset := DirectContainingTrustBoundaryMappedByTechnicalAssetId[what.TargetId]
+	return trustBoundaryOfSourceAsset.Id != trustBoundaryOfTargetAsset.Id
+}
+
+func (what CommunicationLink) IsAcrossTrustBoundaryNetworkOnly() bool {
+	trustBoundaryOfSourceAsset := DirectContainingTrustBoundaryMappedByTechnicalAssetId[what.SourceId]
+	if !trustBoundaryOfSourceAsset.Type.IsNetworkBoundary() { // find and use the parent boundary then
+		trustBoundaryOfSourceAsset = ParsedModelRoot.TrustBoundaries[trustBoundaryOfSourceAsset.ParentTrustBoundaryID()]
+	}
+	trustBoundaryOfTargetAsset := DirectContainingTrustBoundaryMappedByTechnicalAssetId[what.TargetId]
+	if !trustBoundaryOfTargetAsset.Type.IsNetworkBoundary() { // find and use the parent boundary then
+		trustBoundaryOfTargetAsset = ParsedModelRoot.TrustBoundaries[trustBoundaryOfTargetAsset.ParentTrustBoundaryID()]
+	}
+	return trustBoundaryOfSourceAsset.Id != trustBoundaryOfTargetAsset.Id && trustBoundaryOfTargetAsset.Type.IsNetworkBoundary()
+}
+
+func (what CommunicationLink) HighestConfidentiality() Confidentiality {
+	highest := Public
+	for _, dataId := range what.DataAssetsSent {
+		dataAsset := ParsedModelRoot.DataAssets[dataId]
+		if dataAsset.Confidentiality > highest {
+			highest = dataAsset.Confidentiality
+		}
+	}
+	for _, dataId := range what.DataAssetsReceived {
+		dataAsset := ParsedModelRoot.DataAssets[dataId]
+		if dataAsset.Confidentiality > highest {
+			highest = dataAsset.Confidentiality
+		}
+	}
+	return highest
+}
+
+func (what CommunicationLink) HighestIntegrity() Criticality {
+	highest := Archive
+	for _, dataId := range what.DataAssetsSent {
+		dataAsset := ParsedModelRoot.DataAssets[dataId]
+		if dataAsset.Integrity > highest {
+			highest = dataAsset.Integrity
+		}
+	}
+	for _, dataId := range what.DataAssetsReceived {
+		dataAsset := ParsedModelRoot.DataAssets[dataId]
+		if dataAsset.Integrity > highest {
+			highest = dataAsset.Integrity
+		}
+	}
+	return highest
+}
+
+func (what CommunicationLink) HighestAvailability() Criticality {
+	highest := Archive
+	for _, dataId := range what.DataAssetsSent {
+		dataAsset := ParsedModelRoot.DataAssets[dataId]
+		if dataAsset.Availability > highest {
+			highest = dataAsset.Availability
+		}
+	}
+	for _, dataId := range what.DataAssetsReceived {
+		dataAsset := ParsedModelRoot.DataAssets[dataId]
+		if dataAsset.Availability > highest {
+			highest = dataAsset.Availability
+		}
+	}
+	return highest
+}
+
+func (what CommunicationLink) DataAssetsSentSorted() []DataAsset {
+	result := make([]DataAsset, 0)
+	for _, assetID := range what.DataAssetsSent {
+		result = append(result, ParsedModelRoot.DataAssets[assetID])
+	}
+	sort.Sort(ByDataAssetTitleSort(result))
+	return result
+}
+
+func (what CommunicationLink) DataAssetsReceivedSorted() []DataAsset {
+	result := make([]DataAsset, 0)
+	for _, assetID := range what.DataAssetsReceived {
+		result = append(result, ParsedModelRoot.DataAssets[assetID])
+	}
+	sort.Sort(ByDataAssetTitleSort(result))
+	return result
 }
