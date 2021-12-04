974ca789f4387d5988312b3d1c158f11b5c748ff
Martin Vesterlund <Otyg@users.noreply.github.com>
2021-12-04 19:23:49 +0100

    Dataformat

--- model/types.go
+++ model/types.go
@@ -73,40 +73,6 @@ type TypeEnum interface {
 	String() string
 }
 
-type DataFormat int
-
-const (
-	JSON DataFormat = iota
-	XML
-	Serialization
-	File
-	CSV
-)
-
-func DataFormatValues() []TypeEnum {
-	return []TypeEnum{
-		JSON,
-		XML,
-		Serialization,
-		File,
-		CSV,
-	}
-}
-
-func (what DataFormat) String() string {
-	// NOTE: maintain list also in schema.json for validation in IDEs
-	return [...]string{"json", "xml", "serialization", "file", "csv"}[what]
-}
-
-func (what DataFormat) Title() string {
-	return [...]string{"JSON", "XML", "Serialization", "File", "CSV"}[what]
-}
-
-func (what DataFormat) Description() string {
-	return [...]string{"JSON marshalled object data", "XML structured data", "Serialization-based object graphs",
-		"File input/uploads", "CSV tabular data"}[what]
-}
-
 type Protocol int
 
 const (
