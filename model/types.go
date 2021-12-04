c60a26fd46ff0a51e5215b55bb4bbc96e894572a
Martin Vesterlund <Otyg@users.noreply.github.com>
2021-12-04 19:21:49 +0100

    Authorization

--- model/types.go
+++ model/types.go
@@ -73,27 +73,6 @@ type TypeEnum interface {
 	String() string
 }
 
-type Authorization int
-
-const (
-	NoneAuthorization Authorization = iota
-	TechnicalUser
-	EnduserIdentityPropagation
-)
-
-func AuthorizationValues() []TypeEnum {
-	return []TypeEnum{
-		NoneAuthorization,
-		TechnicalUser,
-		EnduserIdentityPropagation,
-	}
-}
-
-func (what Authorization) String() string {
-	// NOTE: maintain list also in schema.json for validation in IDEs
-	return [...]string{"none", "technical-user", "enduser-identity-propagation"}[what]
-}
-
 type Authentication int
 
 const (
