319078d0c46db512a20e55ee990bdcc6a90164a1
Martin Vesterlund <Otyg@users.noreply.github.com>
2021-12-06 10:37:48 +0100

    TechnicalAsset

--- model/types.go
+++ model/types.go
@@ -114,86 +114,6 @@ func (what ByDataAssetDataBreachProbabilityAndTitleSortStillAtRisk) Less(i, j in
 	return highestDataBreachProbabilityLeft > highestDataBreachProbabilityRight
 }
 
-type ByOrderAndIdSort []TechnicalAsset
-
-func (what ByOrderAndIdSort) Len() int      { return len(what) }
-func (what ByOrderAndIdSort) Swap(i, j int) { what[i], what[j] = what[j], what[i] }
-func (what ByOrderAndIdSort) Less(i, j int) bool {
-	if what[i].DiagramTweakOrder == what[j].DiagramTweakOrder {
-		return what[i].Id > what[j].Id
-	}
-	return what[i].DiagramTweakOrder < what[j].DiagramTweakOrder
-}
-
-type ByTechnicalAssetRiskSeverityAndTitleSortStillAtRisk []TechnicalAsset
-
-func (what ByTechnicalAssetRiskSeverityAndTitleSortStillAtRisk) Len() int { return len(what) }
-func (what ByTechnicalAssetRiskSeverityAndTitleSortStillAtRisk) Swap(i, j int) {
-	what[i], what[j] = what[j], what[i]
-}
-func (what ByTechnicalAssetRiskSeverityAndTitleSortStillAtRisk) Less(i, j int) bool {
-	risksLeft := ReduceToOnlyStillAtRisk(what[i].GeneratedRisks())
-	risksRight := ReduceToOnlyStillAtRisk(what[j].GeneratedRisks())
-	highestSeverityLeft := HighestSeverityStillAtRisk(risksLeft)
-	highestSeverityRight := HighestSeverityStillAtRisk(risksRight)
-	var result bool
-	if highestSeverityLeft == highestSeverityRight {
-		if len(risksLeft) == 0 && len(risksRight) > 0 {
-			return false
-		} else if len(risksLeft) > 0 && len(risksRight) == 0 {
-			return true
-		} else {
-			result = what[i].Title < what[j].Title
-		}
-	} else {
-		result = highestSeverityLeft > highestSeverityRight
-	}
-	if what[i].OutOfScope && what[j].OutOfScope {
-		result = what[i].Title < what[j].Title
-	} else if what[i].OutOfScope {
-		result = false
-	} else if what[j].OutOfScope {
-		result = true
-	}
-	return result
-}
-
-type ByTechnicalAssetRAAAndTitleSort []TechnicalAsset
-
-func (what ByTechnicalAssetRAAAndTitleSort) Len() int      { return len(what) }
-func (what ByTechnicalAssetRAAAndTitleSort) Swap(i, j int) { what[i], what[j] = what[j], what[i] }
-func (what ByTechnicalAssetRAAAndTitleSort) Less(i, j int) bool {
-	raaLeft := what[i].RAA
-	raaRight := what[j].RAA
-	if raaLeft == raaRight {
-		return what[i].Title < what[j].Title
-	}
-	return raaLeft > raaRight
-}
-
-/*
-type ByTechnicalAssetQuickWinsAndTitleSort []TechnicalAsset
-
-func (what ByTechnicalAssetQuickWinsAndTitleSort) Len() int      { return len(what) }
-func (what ByTechnicalAssetQuickWinsAndTitleSort) Swap(i, j int) { what[i], what[j] = what[j], what[i] }
-func (what ByTechnicalAssetQuickWinsAndTitleSort) Less(i, j int) bool {
-	qwLeft := what[i].QuickWins()
-	qwRight := what[j].QuickWins()
-	if qwLeft == qwRight {
-		return what[i].Title < what[j].Title
-	}
-	return qwLeft > qwRight
-}
-*/
-
-type ByTechnicalAssetTitleSort []TechnicalAsset
-
-func (what ByTechnicalAssetTitleSort) Len() int      { return len(what) }
-func (what ByTechnicalAssetTitleSort) Swap(i, j int) { what[i], what[j] = what[j], what[i] }
-func (what ByTechnicalAssetTitleSort) Less(i, j int) bool {
-	return what[i].Title < what[j].Title
-}
-
 type ByTrustBoundaryTitleSort []TrustBoundary
 
 func (what ByTrustBoundaryTitleSort) Len() int      { return len(what) }
@@ -240,15 +160,6 @@ func (what ByTechnicalCommunicationLinkTitleSort) Len() int      { return len(wh
 func (what ByTechnicalCommunicationLinkTitleSort) Swap(i, j int) { what[i], what[j] = what[j], what[i] }
 func (what ByTechnicalCommunicationLinkTitleSort) Less(i, j int) bool {
 	return what[i].Title > what[j].Title
-}
-
-func SortedTechnicalAssetIDs() []string {
-	res := make([]string, 0)
-	for id, _ := range ParsedModelRoot.TechnicalAssets {
-		res = append(res, id)
-	}
-	sort.Strings(res)
-	return res
 }
 
 func TagsActuallyUsed() []string {
@@ -318,24 +229,6 @@ func SortedKeysOfDataAssets() []string {
 }
 
 // as in Go ranging over map is random order, range over them in sorted (hence reproducible) way:
-func SortedKeysOfTechnicalAssets() []string {
-	keys := make([]string, 0)
-	for k, _ := range ParsedModelRoot.TechnicalAssets {
-		keys = append(keys, k)
-	}
-	sort.Strings(keys)
-	return keys
-}
-
-func TechnicalAssetsTaggedWithAny(tags ...string) []TechnicalAsset {
-	result := make([]TechnicalAsset, 0)
-	for _, candidate := range ParsedModelRoot.TechnicalAssets {
-		if candidate.IsTaggedWithAny(tags...) {
-			result = append(result, candidate)
-		}
-	}
-	return result
-}
 
 func CommunicationLinksTaggedWithAny(tags ...string) []CommunicationLink {
 	result := make([]CommunicationLink, 0)
@@ -380,14 +273,6 @@ func SharedRuntimesTaggedWithAny(tags ...string) []SharedRuntime {
 }
 
 // as in Go ranging over map is random order, range over them in sorted (hence reproducible) way:
-func SortedTechnicalAssetsByTitle() []TechnicalAsset {
-	assets := make([]TechnicalAsset, 0)
-	for _, asset := range ParsedModelRoot.TechnicalAssets {
-		assets = append(assets, asset)
-	}
-	sort.Sort(ByTechnicalAssetTitleSort(assets))
-	return assets
-}
 
 // as in Go ranging over map is random order, range over them in sorted (hence reproducible) way:
 func SortedDataAssetsByTitle() []DataAsset {
@@ -420,49 +305,6 @@ func SortedDataAssetsByDataBreachProbabilityAndTitle() []DataAsset {
 }
 
 // as in Go ranging over map is random order, range over them in sorted (hence reproducible) way:
-func SortedTechnicalAssetsByRiskSeverityAndTitle() []TechnicalAsset {
-	assets := make([]TechnicalAsset, 0)
-	for _, asset := range ParsedModelRoot.TechnicalAssets {
-		assets = append(assets, asset)
-	}
-	sort.Sort(ByTechnicalAssetRiskSeverityAndTitleSortStillAtRisk(assets))
-	return assets
-}
-
-// as in Go ranging over map is random order, range over them in sorted (hence reproducible) way:
-func SortedTechnicalAssetsByRAAAndTitle() []TechnicalAsset {
-	assets := make([]TechnicalAsset, 0)
-	for _, asset := range ParsedModelRoot.TechnicalAssets {
-		assets = append(assets, asset)
-	}
-	sort.Sort(ByTechnicalAssetRAAAndTitleSort(assets))
-	return assets
-}
-
-/*
-// as in Go ranging over map is random order, range over them in sorted (hence reproducible) way:
-func SortedTechnicalAssetsByQuickWinsAndTitle() []TechnicalAsset {
-	assets := make([]TechnicalAsset, 0)
-	for _, asset := range ParsedModelRoot.TechnicalAssets {
-		if !asset.OutOfScope && asset.QuickWins() > 0 {
-			assets = append(assets, asset)
-		}
-	}
-	sort.Sort(ByTechnicalAssetQuickWinsAndTitleSort(assets))
-	return assets
-}
-*/
-
-func OutOfScopeTechnicalAssets() []TechnicalAsset {
-	assets := make([]TechnicalAsset, 0)
-	for _, asset := range ParsedModelRoot.TechnicalAssets {
-		if asset.OutOfScope {
-			assets = append(assets, asset)
-		}
-	}
-	sort.Sort(ByTechnicalAssetTitleSort(assets))
-	return assets
-}
 
 // as in Go ranging over map is random order, range over them in sorted (hence reproducible) way:
 func SortedKeysOfTrustBoundaries() []string {
@@ -527,37 +369,6 @@ func (what CommunicationLink) DetermineArrowLineStyle() string {
 	return "solid"
 }
 
-// dotted when model forgery attempt (i.e. nothing being processed or stored)
-func (what TechnicalAsset) DetermineShapeBorderLineStyle() string {
-	if len(what.DataAssetsProcessed) == 0 && len(what.DataAssetsStored) == 0 || what.OutOfScope {
-		return "dotted" // dotted, because it's strange when too many technical communication links transfer no data... some ok, but many in a diagram ist a sign of model forgery...
-	}
-	return "solid"
-}
-
-// 3 when redundant
-func (what TechnicalAsset) DetermineShapePeripheries() int {
-	if what.Redundant {
-		return 2
-	}
-	return 1
-}
-
-func (what TechnicalAsset) DetermineShapeStyle() string {
-	return "filled"
-}
-
-func (what TechnicalAsset) GetTrustBoundaryId() string {
-	for _, trustBoundary := range ParsedModelRoot.TrustBoundaries {
-		for _, techAssetInside := range trustBoundary.TechnicalAssetsInside {
-			if techAssetInside == what.Id {
-				return trustBoundary.Id
-			}
-		}
-	}
-	return ""
-}
-
 // Pen Widths:
 
 func (what CommunicationLink) DetermineArrowPenWidth() string {
@@ -569,57 +380,6 @@ func (what CommunicationLink) DetermineArrowPenWidth() string {
 	}
 	return fmt.Sprintf("%f", 1.5)
 }
-
-func (what TechnicalAsset) DetermineShapeBorderPenWidth() string {
-	if what.DetermineShapeBorderColor() == colors.Pink {
-		return fmt.Sprintf("%f", 3.5)
-	}
-	if what.DetermineShapeBorderColor() != colors.Black {
-		return fmt.Sprintf("%f", 3.0)
-	}
-	return fmt.Sprintf("%f", 2.0)
-}
-
-/*
-// Loops over all data assets (stored and processed by this technical asset) and determines for each
-// data asset, how many percentage of the data risk is reduced when this technical asset has all risks mitigated.
-// Example: This means if the data asset is loosing a risk and thus getting from red to amber it counts as 1.
-// Other example: When only one out of four lines (see data risk mapping) leading to red tech assets are removed by
-// the mitigations, then this counts as 0.25. The overall sum is returned.
-func (what TechnicalAsset) QuickWins() float64 {
-	result := 0.0
-	uniqueDataAssetsStoredAndProcessed := make(map[string]interface{})
-	for _, dataAssetId := range what.DataAssetsStored {
-		uniqueDataAssetsStoredAndProcessed[dataAssetId] = true
-	}
-	for _, dataAssetId := range what.DataAssetsProcessed {
-		uniqueDataAssetsStoredAndProcessed[dataAssetId] = true
-	}
-	highestSeverity := HighestSeverityStillAtRisk(what.GeneratedRisks())
-	for dataAssetId, _ := range uniqueDataAssetsStoredAndProcessed {
-		dataAsset := ParsedModelRoot.DataAssets[dataAssetId]
-		if dataAsset.IdentifiedRiskSeverityStillAtRisk() <= highestSeverity {
-			howManySameLevelCausingUsagesOfThisData := 0.0
-			for techAssetId, risks := range dataAsset.IdentifiedRisksByResponsibleTechnicalAssetId() {
-				if !ParsedModelRoot.TechnicalAssets[techAssetId].OutOfScope {
-					for _, risk := range risks {
-						if len(risk.MostRelevantTechnicalAssetId) > 0 { // T O D O caching of generated risks inside the method?
-							if HighestSeverityStillAtRisk(ParsedModelRoot.TechnicalAssets[risk.MostRelevantTechnicalAssetId].GeneratedRisks()) == highestSeverity {
-								howManySameLevelCausingUsagesOfThisData++
-								break
-							}
-						}
-					}
-				}
-			}
-			if howManySameLevelCausingUsagesOfThisData > 0 {
-				result += 1.0 / howManySameLevelCausingUsagesOfThisData
-			}
-		}
-	}
-	return result
-}
-*/
 
 func (what CommunicationLink) IsBidirectional() bool {
 	return len(what.DataAssetsSent) > 0 && len(what.DataAssetsReceived) > 0
@@ -644,136 +404,6 @@ func ContainsCaseInsensitiveAny(a []string, x ...string) bool {
 		}
 	}
 	return false
-}
-
-func (what TechnicalAsset) IsZero() bool {
-	return len(what.Id) == 0
-}
-
-func (what TechnicalAsset) ProcessesOrStoresDataAsset(dataAssetId string) bool {
-	if Contains(what.DataAssetsProcessed, dataAssetId) {
-		return true
-	}
-	if Contains(what.DataAssetsStored, dataAssetId) {
-		return true
-	}
-	return false
-}
-
-// red when >= confidential data stored in unencrypted technical asset
-func (what TechnicalAsset) DetermineLabelColor() string {
-	// TODO: Just move into main.go and let the generated risk determine the color, don't duplicate the logic here
-	// Check for red
-	if what.Integrity == MissionCritical {
-		return colors.Red
-	}
-	for _, storedDataAsset := range what.DataAssetsStored {
-		if ParsedModelRoot.DataAssets[storedDataAsset].Integrity == MissionCritical {
-			return colors.Red
-		}
-	}
-	for _, processedDataAsset := range what.DataAssetsProcessed {
-		if ParsedModelRoot.DataAssets[processedDataAsset].Integrity == MissionCritical {
-			return colors.Red
-		}
-	}
-	// Check for amber
-	if what.Integrity == Critical {
-		return colors.Amber
-	}
-	for _, storedDataAsset := range what.DataAssetsStored {
-		if ParsedModelRoot.DataAssets[storedDataAsset].Integrity == Critical {
-			return colors.Amber
-		}
-	}
-	for _, processedDataAsset := range what.DataAssetsProcessed {
-		if ParsedModelRoot.DataAssets[processedDataAsset].Integrity == Critical {
-			return colors.Amber
-		}
-	}
-	return colors.Black
-	/*
-		if what.Encrypted {
-			return colors.Black
-		} else {
-			if what.Confidentiality == StrictlyConfidential {
-				return colors.Red
-			}
-			for _, storedDataAsset := range what.DataAssetsStored {
-				if ParsedModelRoot.DataAssets[storedDataAsset].Confidentiality == StrictlyConfidential {
-					return colors.Red
-				}
-			}
-			if what.Confidentiality == Confidential {
-				return colors.Amber
-			}
-			for _, storedDataAsset := range what.DataAssetsStored {
-				if ParsedModelRoot.DataAssets[storedDataAsset].Confidentiality == Confidential {
-					return colors.Amber
-				}
-			}
-			return colors.Black
-		}
-	*/
-}
-
-// red when mission-critical integrity, but still unauthenticated (non-readonly) channels access it
-// amber when critical integrity, but still unauthenticated (non-readonly) channels access it
-// pink when model forgery attempt (i.e. nothing being processed or stored)
-func (what TechnicalAsset) DetermineShapeBorderColor() string {
-	// TODO: Just move into main.go and let the generated risk determine the color, don't duplicate the logic here
-	// Check for red
-	if what.Confidentiality == StrictlyConfidential {
-		return colors.Red
-	}
-	for _, storedDataAsset := range what.DataAssetsStored {
-		if ParsedModelRoot.DataAssets[storedDataAsset].Confidentiality == StrictlyConfidential {
-			return colors.Red
-		}
-	}
-	for _, processedDataAsset := range what.DataAssetsProcessed {
-		if ParsedModelRoot.DataAssets[processedDataAsset].Confidentiality == StrictlyConfidential {
-			return colors.Red
-		}
-	}
-	// Check for amber
-	if what.Confidentiality == Confidential {
-		return colors.Amber
-	}
-	for _, storedDataAsset := range what.DataAssetsStored {
-		if ParsedModelRoot.DataAssets[storedDataAsset].Confidentiality == Confidential {
-			return colors.Amber
-		}
-	}
-	for _, processedDataAsset := range what.DataAssetsProcessed {
-		if ParsedModelRoot.DataAssets[processedDataAsset].Confidentiality == Confidential {
-			return colors.Amber
-		}
-	}
-	return colors.Black
-	/*
-		if what.Integrity == MissionCritical {
-			for _, dataFlow := range IncomingTechnicalCommunicationLinksMappedByTargetId[what.Id] {
-				if !dataFlow.Readonly && dataFlow.Authentication == NoneAuthentication {
-					return colors.Red
-				}
-			}
-		}
-
-		if what.Integrity == Critical {
-			for _, dataFlow := range IncomingTechnicalCommunicationLinksMappedByTargetId[what.Id] {
-				if !dataFlow.Readonly && dataFlow.Authentication == NoneAuthentication {
-					return colors.Amber
-				}
-			}
-		}
-
-		if len(what.DataAssetsProcessed) == 0 && len(what.DataAssetsStored) == 0 {
-			return colors.Pink // pink, because it's strange when too many technical assets process no data... some ok, but many in a diagram ist a sign of model forgery...
-		}
-
-		return colors.Black
-	*/
 }
 
 func (what CommunicationLink) DetermineLabelColor() string {
@@ -876,31 +506,6 @@ func (what CommunicationLink) DetermineArrowColor() string {
 			return colors.Black
 		}
 	*/
-}
-
-func (what TechnicalAsset) DetermineShapeFillColor() string {
-	fillColor := colors.VeryLightGray
-	if len(what.DataAssetsProcessed) == 0 && len(what.DataAssetsStored) == 0 ||
-		what.Technology == UnknownTechnology {
-		fillColor = colors.LightPink // lightPink, because it's strange when too many technical assets process no data... some ok, but many in a diagram ist a sign of model forgery...
-	} else if len(what.CommunicationLinks) == 0 && len(IncomingTechnicalCommunicationLinksMappedByTargetId[what.Id]) == 0 {
-		fillColor = colors.LightPink
-	} else if what.Internet {
-		fillColor = colors.ExtremeLightBlue
-	} else if what.OutOfScope {
-		fillColor = colors.OutOfScopeFancy
-	} else if what.CustomDevelopedParts {
-		fillColor = colors.CustomDevelopedParts
-	}
-	switch what.Machine {
-	case Physical:
-		fillColor = colors.DarkenHexColor(fillColor)
-	case Container:
-		fillColor = colors.BrightenHexColor(fillColor)
-	case Serverless:
-		fillColor = colors.BrightenHexColor(colors.BrightenHexColor(fillColor))
-	}
-	return fillColor
 }
 
 type ByRiskCategoryTitleSort []RiskCategory
@@ -1661,16 +1266,6 @@ func HighestExploitationImpact(risks []Risk) RiskExploitationImpact {
 	for _, risk := range risks {
 		if risk.ExploitationImpact > result {
 			result = risk.ExploitationImpact
-		}
-	}
-	return result
-}
-
-func InScopeTechnicalAssets() []TechnicalAsset {
-	result := make([]TechnicalAsset, 0)
-	for _, asset := range ParsedModelRoot.TechnicalAssets {
-		if !asset.OutOfScope {
-			result = append(result, asset)
 		}
 	}
 	return result
