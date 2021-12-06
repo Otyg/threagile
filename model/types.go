2267d24df31a0cf2b57fbc1fa38d785f26b7626d
Martin Vesterlund <Otyg@users.noreply.github.com>
2021-12-06 09:38:18 +0100

    Risk

--- model/types.go
+++ model/types.go
@@ -949,47 +949,6 @@ type RiskStatistics struct {
 	Risks map[string]map[string]int `json:"risks"`
 }
 
-type Risk struct {
-	Category                        RiskCategory               `json:"-"`           // just for navigational convenience... not JSON marshalled
-	CategoryId                      string                     `json:"category"`    // used for better JSON marshalling, is assigned in risk evaluation phase automatically
-	RiskStatus                      RiskStatus                 `json:"risk_status"` // used for better JSON marshalling, is assigned in risk evaluation phase automatically
-	Severity                        RiskSeverity               `json:"severity"`
-	ExploitationLikelihood          RiskExploitationLikelihood `json:"exploitation_likelihood"`
-	ExploitationImpact              RiskExploitationImpact     `json:"exploitation_impact"`
-	Title                           string                     `json:"title"`
-	SyntheticId                     string                     `json:"synthetic_id"`
-	MostRelevantDataAssetId         string                     `json:"most_relevant_data_asset"`
-	MostRelevantTechnicalAssetId    string                     `json:"most_relevant_technical_asset"`
-	MostRelevantTrustBoundaryId     string                     `json:"most_relevant_trust_boundary"`
-	MostRelevantSharedRuntimeId     string                     `json:"most_relevant_shared_runtime"`
-	MostRelevantCommunicationLinkId string                     `json:"most_relevant_communication_link"`
-	DataBreachProbability           DataBreachProbability      `json:"data_breach_probability"`
-	DataBreachTechnicalAssetIDs     []string                   `json:"data_breach_technical_assets"`
-	// TODO: refactor all "Id" here to "ID"?
-}
-
-func (what Risk) GetRiskTracking() RiskTracking { // TODO: Unify function naming reagrding Get etc.
-	var result RiskTracking
-	if riskTracking, ok := ParsedModelRoot.RiskTracking[what.SyntheticId]; ok {
-		result = riskTracking
-	}
-	return result
-}
-
-func (what Risk) GetRiskTrackingStatusDefaultingUnchecked() RiskStatus {
-	if riskTracking, ok := ParsedModelRoot.RiskTracking[what.SyntheticId]; ok {
-		return riskTracking.Status
-	}
-	return Unchecked
-}
-
-func (what Risk) IsRiskTracked() bool {
-	if _, ok := ParsedModelRoot.RiskTracking[what.SyntheticId]; ok {
-		return true
-	}
-	return false
-}
-
 type ByRiskSeveritySort []Risk
 
 func (what ByRiskSeveritySort) Len() int { return len(what) }
