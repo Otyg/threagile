2e5e3ed06725d71dff51b53b46343bd37c15fe5d
Martin Vesterlund <Otyg@users.noreply.github.com>
2021-12-06 09:37:19 +0100

    RiskCategory

---
+++ model/riskCategory.go
@@ -0,0 +1,22 @@
+package model
+
+type RiskCategory struct {
+	// TODO: refactor all "Id" here and elsewhere to "ID"
+	Id                         string
+	Title                      string
+	Description                string
+	Impact                     string
+	ASVS                       string
+	CheatSheet                 string
+	TestingGuide               string
+	Action                     string
+	Mitigation                 string
+	Check                      string
+	DetectionLogic             string
+	RiskAssessment             string
+	FalsePositives             string
+	Function                   RiskFunction
+	STRIDE                     STRIDE
+	ModelFailurePossibleReason bool
+	CWE                        int
+}
--- model/types.go
+++ model/types.go
@@ -911,27 +911,6 @@ func (what TechnicalAsset) DetermineShapeFillColor() string {
 	return fillColor
 }
 
-type RiskCategory struct {
-	// TODO: refactor all "Id" here and elsewhere to "ID"
-	Id                         string
-	Title                      string
-	Description                string
-	Impact                     string
-	ASVS                       string
-	CheatSheet                 string
-	TestingGuide               string
-	Action                     string
-	Mitigation                 string
-	Check                      string
-	DetectionLogic             string
-	RiskAssessment             string
-	FalsePositives             string
-	Function                   RiskFunction
-	STRIDE                     STRIDE
-	ModelFailurePossibleReason bool
-	CWE                        int
-}
-
 type ByRiskCategoryTitleSort []RiskCategory
 
 func (what ByRiskCategoryTitleSort) Len() int { return len(what) }
