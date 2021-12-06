3da819fa619f65e87ad00f69c92f298ce12c459a
Martin Vesterlund <Otyg@users.noreply.github.com>
2021-12-06 10:17:56 +0100

    RiskCategory

--- model/riskCategory.go
+++ model/riskCategory.go
@@ -20,3 +20,23 @@ type RiskCategory struct {
 	ModelFailurePossibleReason bool
 	CWE                        int
 }
+
+type InputIndividualRiskCategory struct {
+	ID                            string                         `json:"id"`
+	Description                   string                         `json:"description"`
+	Impact                        string                         `json:"impact"`
+	ASVS                          string                         `json:"asvs"`
+	Cheat_sheet                   string                         `json:"cheat_sheet"`
+	Testing_guide                 string                         `json:"testing_guide"`
+	Action                        string                         `json:"action"`
+	Mitigation                    string                         `json:"mitigation"`
+	Check                         string                         `json:"check"`
+	Function                      string                         `json:"function"`
+	STRIDE                        string                         `json:"stride"`
+	Detection_logic               string                         `json:"detection_logic"`
+	Risk_assessment               string                         `json:"risk_assessment"`
+	False_positives               string                         `json:"false_positives"`
+	Model_failure_possible_reason bool                           `json:"model_failure_possible_reason"`
+	CWE                           int                            `json:"cwe"`
+	Risks_identified              map[string]InputRiskIdentified `json:"risks_identified"`
+}
