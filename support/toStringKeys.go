14f6676c0163555738d3a57e534008106df4737f
Martin Vesterlund <Otyg@users.noreply.github.com>
2022-01-13 15:00:30 +0100

    Fix some issues (#74)

--- support/toStringKeys.go
+++ support/toStringKeys.go
@@ -2,13 +2,11 @@ package support
 
 import (
 	"errors"
-	"fmt"
 	"time"
 )
 
 func ToStringKeys(val interface{}) (interface{}, error) {
 	var err error
-	fmt.Printf("val: %T\n", val)
 	switch val := val.(type) {
 	case map[string]interface{}:
 		s := make(map[string]interface{})
@@ -44,7 +42,6 @@ func ToStringKeys(val interface{}) (interface{}, error) {
 	case time.Time:
 		return val.Format("2006-01-02"), nil
 	default:
-		fmt.Println("In default")
 		return val, nil
 	}
 }
